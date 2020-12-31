bd44d51a934365fbf5836d9b423db5fb9ece8544
Karris  Black <karris.b@gmail.com>
2020-12-31 01:19:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,10 @@ def square_array(number)
   i = 0 
   result = []
   while i < number.length
-  result.push(number[i]*[i])
+  result.push(number[i]*number[i])
   i += 1
   end
+  return result
 end  
  
  
